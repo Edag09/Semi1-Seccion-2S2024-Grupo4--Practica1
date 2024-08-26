fdca200f822f6bc6325875864d6b9029b56ac3b6
Brayan-PM <78266710+BrayanPradoMarroquin@users.noreply.github.com>
2024-08-25 18:53:32 -0600

    [ADD] se creo la conexion con la base de datos

---
+++ backend/server_py/connectbd.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+import mysql.connector
+
+connect = mysql.connector.connect(
+    user='admin', 
+    password='*Semi1_Practica1*', 
+    host='bdpractica1.cp842gwg2jsl.us-east-1.rds.amazonaws.com', 
+    database='Practica1_semi', 
+    port='3306'
+)
+
+
