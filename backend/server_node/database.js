c889f6824fe58759db2be780774ae8ef24bbd22c
Pablo Jose Oliva Bonilla <Pabloolivaa@gmail.com>
2024-08-31 20:57:06 -0600

    [update] Se corrigio la conexion a la base de datos

--- backend/server_node/database.js
+++ backend/server_node/database.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const mysqlConnection = mysql.createConnection({
-  host: process.env["bdpractica1.cp842gwg2jsl.us-east-1.rds.amazonaws.com"],
-  user: process.env["admin"],
-  password: process.env["*Semi1_Practica1*"],
-  database: process.env["Practica1_semi"],
+  host: "bdpractica1.cp842gwg2jsl.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "*Semi1_Practica1*",
+  database: "Practica1_semi",
 });
 
 mysqlConnection.connect(function (err) {
