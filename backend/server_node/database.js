7a8cb4a9713a6fdbf8b5f46e89d3640bb0666380
Pablo Jose Oliva Bonilla <Pabloolivaa@gmail.com>
2024-08-31 20:47:25 -0600

    [update] Modificacion de conexion de node

--- backend/server_node/database.js
+++ backend/server_node/database.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const mysqlConnection = mysql.createConnection({
-    host: process.env["DB_HOST"],
-    user: process.env["DB_USER"],
-    password: process.env["DB_PASSWORD"],
-    database: process.env["DB_SCHEMA"]
+  host: process.env["bdpractica1.cp842gwg2jsl.us-east-1.rds.amazonaws.com"],
+  user: process.env["admin"],
+  password: process.env["*Semi1_Practica1*"],
+  database: process.env["Practica1_semi"],
 });
 
 mysqlConnection.connect(function (err) {
-    if(err){
-        console.log(err);
-        return;
-    }else{
-        console.log("Base de Datos conectada")
-    }
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("Base de Datos conectada");
+  }
 });
 
 module.exports = mysqlConnection;
