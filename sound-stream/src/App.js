53612fa34c59447bf7c639a0efab43929d76a550
Eduardo Agustin <3006840880101@ingenieria.usac.edu.gt>
2024-08-31 18:07:04 -0600

    [UPDATE] Frontend changes

--- sound-stream/src/App.js
+++ sound-stream/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Registro from './Components/Registro/Registro';
+import Inicio from './Components/Principal/Inicio';
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/" element={<Inicio/>}/>
       </Routes>
     </Router>
   );
